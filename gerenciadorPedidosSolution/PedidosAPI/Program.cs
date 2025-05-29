a4b0b11c45e3df856a55ebd3664d3863e2ad8253
Sergio Manuca <ssergioca@gmail.com>
2025-05-29 17:12:50 -0300

    fix: Permissão dos CORS. Injetando suas dependicas

--- gerenciadorPedidosSolution/PedidosAPI/Program.cs
+++ gerenciadorPedidosSolution/PedidosAPI/Program.cs
@@ -20,6 +20,15 @@ builder.Services.AddScoped<IProdutoService, ProdutoService>();
 builder.Services.AddScoped<IPedidoService, PedidoService>();
 builder.Services.AddScoped<IPedidoRepository, PedidoRepository>();
 builder.Services.AddScoped<IPedidoItemRepository, PedidoItemRepository>();
+
+builder.Services.AddCors(options => {
+    options.AddPolicy("AllowMyOrigin", builder => {
+        builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+        builder.WithOrigins("http://127.0.0.1:5500")
+                   .AllowAnyHeader()
+                   .AllowAnyMethod();
+    });
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -28,7 +37,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseCors("AllowMyOrigin");
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
